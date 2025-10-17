8ed26457e859160cfe7b48e3e41d8d60dd2e43fc
MoustaidHind <151015622+MoustaidHind@users.noreply.github.com>
2025-10-17 23:40:26 +0100

    start ex00

---
+++ Module_01/ex00/Zombie.cpp
@@ -0,0 +1,27 @@
+#include "Zombie.hpp"
+
+
+void Zombie::announce(void)
+{
+	std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+Zombie* Zombie::newZombie(std::string name)
+{
+
+}
+
+void Zombie::randomChump(std::string name)
+{
+
+}
+
+Zombie::Zombie()
+{
+	
+}
+
+Zombie::~Zombie()
+{
+
+}
---
+++ Module_01/ex00/Zombie.hpp
@@ -0,0 +1,21 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+
+class Zombie
+{
+private:
+	std::string name;
+
+
+public:
+	Zombie();
+	void	announce(void);
+	void	randomChump( std::string name );
+	Zombie* newZombie( std::string name );
+	~Zombie();
+
+};
+
+#endif

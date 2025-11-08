1522d422764240a19a060d34a0fa568df5f13642
MoustaidHind <151015622+MoustaidHind@users.noreply.github.com>
2025-11-08 18:25:00 +0100

    module01/ex03 setup

---
+++ Module_01/ex03/HumanA.hpp
@@ -0,0 +1,18 @@
+#ifndef HUMANA_HPP
+#define HUMANA_HPP
+
+
+
+
+class HumanA
+{
+private:
+
+
+public:
+
+
+
+};
+
+#endif
---
+++ Module_01/ex03/HumanB.hpp
@@ -0,0 +1,18 @@
+#ifndef HUMANB_HPP
+#define HUMANB_HPP
+
+
+
+
+class HumanB
+{
+private:
+
+
+public:
+
+
+
+};
+
+#endif

4e33b78e97a495cca9d90794184bd40080381fc7
MoustaidHind <151015622+MoustaidHind@users.noreply.github.com>
2025-12-05 16:59:31 +0100

    module02/ex00 done

--- Module_02/ex00/Fixed.hpp
+++ Module_02/ex00/Fixed.hpp
@@ -5,6 +5,20 @@
 #include <iostream>
 
 
+class Fixed
+{
+private:
+	static const int	_fractional_bits; // store the number of fractional bits. Its value will always be the integer literal 8.
+	int					_value; // An integer to store the fixed-point number value.
 
+public:
+	Fixed(); // default constructor
+	Fixed(const Fixed& other); // copy constructor
+	Fixed& operator = (const Fixed& other); // copy assignment operator overload
+	~Fixed(); // destructor
+
+	int getRawBits( void ) const; // returns the raw value of the fixed-point value
+	void setRawBits( int const raw ); // that sets the raw value of the fixed-point number
+};
 
 #endif
--- Module_02/ex00/main.cpp
+++ Module_02/ex00/main.cpp
@@ -1,1 +1,16 @@
 #include "Fixed.hpp"
+
+int main( void ) 
+{
+	Fixed a; //def
+	Fixed b( a ); //copy -> get
+	Fixed c; // def
+
+	c = b; // copy assi -> get
+
+	std::cout << a.getRawBits() << std::endl; // get
+	std::cout << b.getRawBits() << std::endl; // get
+	std::cout << c.getRawBits() << std::endl; // get
+
+	return 0;
+}

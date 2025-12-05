7ce7936f8924223a86cc5ea1c74154d72fe39431
MoustaidHind <151015622+MoustaidHind@users.noreply.github.com>
2025-12-05 17:03:04 +0100

    module02/ex01 setup

---
+++ Module_02/ex01/Fixed.cpp
@@ -0,0 +1,47 @@
+#include "Fixed.hpp"
+
+const int	Fixed::_fractional_bits = 8;
+
+Fixed::Fixed() : _value(0) // default constructor
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& other) //: _value(other.getRawBits()) // copy constructor
+{
+	std::cout << "Copy constructor called" << std::endl;
+	_value = other.getRawBits();
+}
+
+Fixed& Fixed::operator= (const Fixed& other)  // copy assignment operator overload
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if(this == &other)
+		return (*this);
+	
+	_value = other.getRawBits();
+	
+	return(*this);
+}
+
+Fixed::~Fixed() // destructor
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+/*
+--> const in the end 
+This member function is not allowed to modify the object.
+It cannot change any attribute of the class.
+It is a read-only function.
+*/
+int Fixed::getRawBits( void ) const // returns the raw value of the fixed-point value
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return(_value);
+}
+
+void Fixed::setRawBits( int const raw ) // that sets the raw value of the fixed-point number
+{
+	_value = raw;
+}

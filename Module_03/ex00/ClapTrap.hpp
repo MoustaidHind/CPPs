508996ae0d7e78d9fc8041acf813a4b268281bfe
MoustaidHind <151015622+MoustaidHind@users.noreply.github.com>
2025-12-19 03:50:05 +0100

    module03/ex00 solved

---
+++ Module_03/ex00/ClapTrap.hpp
@@ -0,0 +1,30 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap
+{
+private:
+	std::string		Name;
+	unsigned int	Hit_points;
+	unsigned int	Energy_points;
+	unsigned int	Attack_points;
+
+public:
+	ClapTrap();
+	ClapTrap(std::string name);
+	ClapTrap(const ClapTrap& other);
+	ClapTrap& operator= (const ClapTrap& other);
+	~ClapTrap();
+
+	void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+};
+
+
+
+
+
+#endif

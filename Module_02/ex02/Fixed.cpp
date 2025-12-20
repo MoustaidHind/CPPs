449924719feb8fcb07e1686e6be0650110891995
MoustaidHind <151015622+MoustaidHind@users.noreply.github.com>
2025-12-20 02:18:16 +0100

    module02/ex02 remove comments

--- Module_02/ex02/Fixed.cpp
+++ Module_02/ex02/Fixed.cpp
@@ -3,70 +3,52 @@
 const int	Fixed::_fractional_bits = 8;
 
 /* CONSTRUCTORS */
-Fixed::Fixed() : _value(0) // default constructor
+Fixed::Fixed() : _value(0) {}
+
+Fixed::Fixed(const Fixed& other)
 {
-	// std::cout << "Default constructor called" << std::endl;
+	*this = other; 
 }
-Fixed::Fixed(const Fixed& other) // copy constructor
+Fixed& Fixed::operator= (const Fixed& other)
 {
-	// std::cout << "Copy constructor called" << std::endl;
-	// _value = other.getRawBits();
-	*this = other; // ->  [[ WHY THAT WORK, WHEN THE OPERATOR= NEEDS TWO EXISTING OBJECTS ]]
-}
-Fixed& Fixed::operator= (const Fixed& other)  // copy assignment operator overload
-{
-	// std::cout << "Copy assignment operator called" << std::endl;
 	if(this == &other)
 		return (*this);	
 	_value = other.getRawBits();
 	return(*this);
 }
-Fixed::~Fixed() // destructor
+Fixed::~Fixed() {}
+
+Fixed::Fixed(const int val)
 {
-	// std::cout << "Destructor called" << std::endl;
-}
-Fixed::Fixed(const int val)// parametrized constructor takes a constant integer as a parameter
-{
-	// std::cout << "Int constructor called" << std::endl;
 	setRawBits(val * (1 << _fractional_bits));
 }
 
-Fixed::Fixed(const float val)// parametrized constructor takes a constant floating-point number
+Fixed::Fixed(const float val)
 {
-	// std::cout << "float constructor called" << std::endl;
 	setRawBits(roundf(val * (1 << _fractional_bits)));
 }
 
-
-
 /* GETTER && SETTER */
-int Fixed::getRawBits( void ) const // returns the raw value of the fixed-point value
+int Fixed::getRawBits( void ) const 
 {
-	/* const in the end 
-	This member function is not allowed to modify the object.
-	It cannot change any attribute of the class.
-	It is a read-only function.
-	*/
 	return(_value);
 }
-void Fixed::setRawBits( int const raw ) // that sets the raw value of the fixed-point number
+void Fixed::setRawBits( int const raw )
 {
 	_value = raw;
 }
 
 
-
 // CONVERTION 
-int Fixed::toInt( void ) const // that converts the fixed-point value to an integer value
+int Fixed::toInt( void ) const
 {
-	return(_value / (1 << _fractional_bits));
+	return(this->getRawBits() / (1 << _fractional_bits));
 }
 
-float Fixed::toFloat( void ) const  // that converts the fixed-point value to an floating-point value
+float Fixed::toFloat( void ) const
 {
-	return((float)_value / (float)(1 << _fractional_bits));
+	return((float)this->getRawBits() / (float)(1 << _fractional_bits));
 }
-
 
 
 // function overloading 
@@ -76,52 +58,48 @@ std::ostream& operator<<(std::ostream &os, const Fixed& other)
 	return(os);
 };
 
-
-
-
 // The 6 comparison operators: >, <, >=, <=, ==, and != 
-bool Fixed::operator>(const Fixed& other) const  // const : (promise to leave this object unchanged)
+bool Fixed::operator>(const Fixed& other) const
 {
-	if(this->_value > other._value)
+	if(this->getRawBits() > other.getRawBits())
 		return(true);
 	return(false);
 }
 
 bool Fixed::operator<(const Fixed& other) const
 {
-	if(this->_value < other._value)
+	if(this->getRawBits() < other.getRawBits())
 		return(true);
 	return(false);
 }
 
 bool Fixed::operator>=(const Fixed& other) const
 {
-	if(this->_value >= other._value)
+	if(this->getRawBits() >= other.getRawBits())
 		return(true);
 	return(false);
 }
 
 bool Fixed::operator<=(const Fixed& other) const
 {
-	if(this->_value <= other._value)
+	if(this->getRawBits() <= other.getRawBits())
 		return(true);
 	return(false);
 }
 
 bool Fixed::operator==(const Fixed& other) const
 {
-	if(this->_value == other._value)
+	if(this->getRawBits() == other.getRawBits())
 		return(true);
 	return(false);
 }
 
 bool Fixed::operator!=(const Fixed& other) const
 {
-	if(this->_value != other._value)
+	if(this->getRawBits() != other.getRawBits())
 		return(true);
 	return(false);
 }
-
 
 
 // The 4 arithmetic operators: +, -, *, and /
@@ -145,38 +123,28 @@ Fixed Fixed::operator* (const Fixed& other) const
 {
 	Fixed multi;
 
-	multi.setRawBits(((long long)this->_value * other._value) / (1 << _fractional_bits));
-	// cast to long long , to prevent overflowing of int
-	// a*2⁸ * b*2⁸ = a * b * 2¹⁶ →→ divide in 2⁸ →→ a * b * 2⁸ [this is the right raw value, then divide in 2⁸ to get real value]
+	multi.setRawBits(((long long)this->getRawBits() * other.getRawBits()) / (1 << _fractional_bits));
 	return(multi);
-
 }
 
 Fixed Fixed::operator/ (const Fixed& other) const
 {
 	Fixed devi;
 
-	devi.setRawBits(((long long)this->_value << _fractional_bits) / other._value);
-	// a*2⁸ / b*2⁸ = a * b * 2⁰(1) →→ multi one value with 2⁸ →→ a * b * 2⁸ [this is the right raw value, then divide in 2⁸ to get real value]
+	devi.setRawBits(((long long)this->getRawBits() * (1 << _fractional_bits)) / other.getRawBits());
 	return(devi);
 }
 
-
-// The 4 increment/decrement (pre-increment and post-increment, pre-decrement and
-// 	post-decrement) operators, which will increase or decrease the fixed-point value by
-// 	the smallest representable ϵ, such that 1 + ϵ > 1.
-
+// The 4 increment/decrement (pre, post)
 Fixed& Fixed::operator++()
 {
-	// Increments immediately, then returns the new value
 	this->_value++;
 	return(*this);
 }
 
-Fixed Fixed::operator++(int) // int parameter to makes the function signature different (post increment)
+Fixed Fixed::operator++(int)
 {
-	// Returns the current value (a copy), then increments afterwards
-	Fixed post_incr(*this); // call copy constructor
+	Fixed post_incr(*this);
 
 	this->_value++;
 	return(post_incr);
@@ -190,53 +158,37 @@ Fixed& Fixed::operator--()
 
 Fixed Fixed::operator--(int)
 {
-	// Returns the current value (a copy), then increments afterwards
-	Fixed post_decr(*this); // call copy constructor
+	Fixed post_decr(*this);
 
 	this->_value--;
 	return(post_decr);
 }
 
-//4 public overloaded STATIC member functions (min, max) with 
-	//(references to fixed-point) and (references to constant fixed-point)
-	// static Fixed& min(Fixed& f1, Fixed& f2);
-	// static Fixed& min(const Fixed& f1, const Fixed& f2);
-	// static Fixed& max(Fixed& f1, Fixed& f2);
-	// static Fixed& max(const Fixed& f1, const Fixed& f2);
-
+//4 public overloaded STATIC member functions (min, max) 
 Fixed& Fixed::min(Fixed& f1, Fixed& f2)
 {
-	if(f1._value > f2._value)
+	if(f1.getRawBits() > f2.getRawBits())
 		return(f2);
 	return(f1);
 }
 
 const Fixed& Fixed::min(const Fixed& f1, const Fixed& f2)
 {
-	if(f1._value > f2._value)
+	if(f1.getRawBits() > f2.getRawBits())
 		return(f2);
 	return(f1);
 }
 
-
 Fixed& Fixed::max(Fixed& f1, Fixed& f2)
 {
-	if(f1._value > f2._value)
+	if(f1.getRawBits() > f2.getRawBits())
 		return(f1);
 	return(f2);
 }
 
 const Fixed& Fixed::max(const Fixed& f1, const Fixed& f2)
 {
-	if(f1._value > f2._value)
+	if(f1.getRawBits() > f2.getRawBits())
 		return(f1);
 	return(f2);
 }
-
-/* 
-why you do const,
-what affect if you dont fix * or + or ...
-where i should shift with 8, and where i should multi by 2⁸
-what about negative numbers, 
-test divide by 0 (the subject deos not require it)
-*/
--- Module_02/ex02/Fixed.hpp
+++ Module_02/ex02/Fixed.hpp
@@ -9,21 +9,21 @@
 class Fixed
 {
 private:
-	static const int	_fractional_bits; // store the number of fractional bits. Its value will always be the integer literal 8.
-	int					_value; // An integer to store the fixed-point number value.
+	int					_value;
+	static const int	_fractional_bits;
 
 public:
-	Fixed(); // default constructor
-	Fixed(const Fixed& other); // copy constructor
-	Fixed(const int val); // parametrized constructor (constant integer)
-	Fixed(const float val); // parametrized constructor (constant floating-point)
-	Fixed& operator = (const Fixed& other); // copy assignment operator overload
-	~Fixed(); // destructor
+	Fixed();
+	Fixed(const Fixed& other);
+	Fixed(const int val);
+	Fixed(const float val);
+	Fixed& operator = (const Fixed& other);
+	~Fixed();
 
-	int getRawBits( void ) const; // returns the raw value of the fixed-point value
-	void setRawBits( int const raw ); // that sets the raw value of the fixed-point number
-	float toFloat( void ) const; // converts the fixed-point value to a floating-point value.
-	int toInt( void ) const; // that converts the fixed-point value to an integer value.
+	int		getRawBits( void ) const;
+	void	setRawBits( int const raw );
+	float	toFloat( void ) const;
+	int		toInt( void ) const;
 
 	// The 6 comparison operators: >, <, >=, <=, ==, and !=
 	bool operator> (const Fixed& other) const;
@@ -39,22 +39,17 @@ public:
 	Fixed operator* (const Fixed& other) const;
 	Fixed operator/ (const Fixed& other) const;
 
-	// The 4 increment/decrement (pre-increment and post-increment, pre-decrement and
-	// 	post-decrement) operators, which will increase or decrease the fixed-point value by
-	// 	the smallest representable ϵ, such that 1 + ϵ > 1.
-	Fixed& operator++(); //pre-inc ++i
-	Fixed operator++(int); //post-inc i++
-	Fixed& operator--(); //pre-dec
-	Fixed operator--(int); //post-dec
+	// The 4 increment/decrement (pre, post)
+	Fixed& operator++();
+	Fixed operator++(int);
+	Fixed& operator--();
+	Fixed operator--(int);
 
-
-	//4 public overloaded STATIC member functions (min, max) with 
-	//(references to fixed-point) and (references to constant fixed-point)
+	//4 public overloaded STATIC member functions (min, max)
 	static Fixed& min(Fixed& f1, Fixed& f2);
 	static const Fixed& min(const Fixed& f1, const Fixed& f2);
 	static Fixed& max(Fixed& f1, Fixed& f2);
 	static const Fixed& max(const Fixed& f1, const Fixed& f2);
-
 };
 
 std::ostream& operator<<(std::ostream &os, const Fixed& other);

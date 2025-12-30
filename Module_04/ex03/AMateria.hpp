30bf63e16bf0844940dec8ea9f917c23edf18cb7
MoustaidHind <151015622+MoustaidHind@users.noreply.github.com>
2025-12-30 17:58:08 +0100

    addded character files

--- Module_04/ex03/AMateria.hpp
+++ Module_04/ex03/AMateria.hpp
@@ -3,6 +3,7 @@
 
 
 #include <iostream>
+#include "ICharacter.hpp"
 
 
 class AMateria
@@ -14,7 +15,7 @@ public:
 	// [...]
 	std::string const & getType() const; //Returns the materia type
 	virtual AMateria* clone() const = 0;
-	// virtual void use(ICharacter& target);
+	virtual void use(ICharacter& target);
 };
 
 

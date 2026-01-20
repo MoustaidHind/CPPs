a2749f53f65ac00a48811c1b5e9379764f3b1393
MoustaidHind <151015622+MoustaidHind@users.noreply.github.com>
2026-01-20 15:54:21 +0100

    solve EX00

--- Module_05/ex00/Bureaucrat.hpp
+++ Module_05/ex00/Bureaucrat.hpp
@@ -9,17 +9,40 @@ private:
 	int					_grade; // 1 (highest possible grade) to 150 (lowest possible grade)
 
 public:
+	// Orthodox
 	Bureaucrat();
+	Bureaucrat(const std::string _name, int _grade);
 	Bureaucrat(const Bureaucrat &src);
 	Bureaucrat &operator=(const Bureaucrat &src);
 	~Bureaucrat();
 
 	// if out of range must throw the same exceptions as the constructor.
-	void	incrementGrade(); 
-	void	decrementGrade();
+	void	incrementGrade();  // substract 2 become 1
+	void	decrementGrade(); // add 3 become 4
 
 	const std::string	getName() const;
 	int					getGrade() const;
+
+	class GradeTooHighException : public std::exception {
+		public :
+		virtual const char* what() const throw()
+		{
+			return "Grade is too High!";
+		}
+	};
+
+	class GradeTooLowException : public std::exception {
+		public :
+		virtual const char* what() const throw()
+		{
+			return "Grade is too Low!";
+		}
+	};
 };
 
 std::ostream &operator<<(std::ostream &os, const Bureaucrat &src); // <name>, bureaucrat grade <grade>
+
+
+// try catch throw. (exception& e)
+// costum exception + virtual function
+// how exceptions work internally

eaa9afbacdbab16c26b07e6aab56ee312b78d820
MoustaidHind <151015622+MoustaidHind@users.noreply.github.com>
2026-04-25 17:00:56 +0100

    module_08/ex02 solved

--- Module_08/ex02/MutantStack.hpp
+++ Module_08/ex02/MutantStack.hpp
@@ -2,17 +2,35 @@
 #define MUTANTSTACK
 
 
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <stack>
 
-class MutantStack
+template <typename T>
+class MutantStack : public std::stack<T>
 {
-private:
-	/* data */
+
 public:
-	MutantStack(/* args */);
-	~MutantStack();
+	
+	typedef typename std::stack<T>::container_type::iterator iterator;
+
+	MutantStack(){};
+	MutantStack(const MutantStack& src) : std::stack<T>(src) {};
+	MutantStack& operator=(const MutantStack& src) 
+	{
+		if(this == &src)
+			return(*this);
+
+		std::stack<T>::operator=(src);
+		return (*this);
+	};
+	~MutantStack(){};
+
+
+	iterator begin() { return( std::stack<T>::c.begin() ); };
+	iterator end() { return( std::stack<T>::c.end() ); };
 };
-
-
 
 
 
